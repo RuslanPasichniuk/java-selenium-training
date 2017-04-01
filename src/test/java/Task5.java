12c03c0ff2738a2eb0a19016dca301a7b9395045
RuslanPasichniuk <ruslan.pasichniuk@gmail.com>
2017-04-01 20:34:26 +0300

    Homework: Task #5

--- src/test/java/Task5.java
+++ src/test/java/Task5.java
@@ -1,74 +1,79 @@
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+import com.google.common.collect.ImmutableMap;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
-
+import java.util.Map;
 import static junit.framework.TestCase.assertEquals;
 
+
+@RunWith(Parameterized.class)
 public class Task5 {
 
-    public static WebDriver browser;
-    public static WebDriverWait wait;
-    String url = "http://172.22.50.10/litecart";
+    @Parameterized.Parameter
+    public  WebDriver browser;
+    public  WebDriverWait wait;
 
-    @BeforeClass
-    public static void start() {
-        ChromeDriverManager.getInstance().setup();
-        browser = new ChromeDriver();
+    String url = "http://localhost/litecart";
+    Map<String, String> colorValue = ImmutableMap.of("greyOnMain", "rgba(119, 119, 119, 1)", "redOnMain", "rgba(204, 0, 0, 1)", "greyOnItem", "rgba(102, 102, 102, 1)", "redOnItem", "rgba(204, 0, 0, 1)");
+
+
+    @Before
+    public void start() {
         wait = new WebDriverWait(browser, 5);
     }
 
     @Test
-    public void gotoStore(){
-        browser.get(url);
-        if(areElementsPresent(By.id("box-login"))){login_as("admin", "admin");}
-        wait.until(ExpectedConditions.titleIs("Online Store | My Store"));
-    }
-
-    @Test
-    public  void compareTwoPages() {
+    public void compareItemOptions(){
         browser.get(url);
         WebElement sectionCampains = browser.findElement(By.id("box-campaigns"));
         //get info for element on mainPage
         String discountMainPrise = sectionCampains.findElement(By.className("regular-price")).getText();
+        String discountMainColor = sectionCampains.findElement(By.className("regular-price")).getCssValue("color");
         String regularMainPrise = sectionCampains.findElement(By.className("campaign-price")).getText();
+        String regularMainColor = sectionCampains.findElement(By.className("campaign-price")).getCssValue("color");
         String itemNameMain = sectionCampains.findElement(By.className("name")).getText();
+
+
         //open item page
-       //!!! browser.findElement(By.tagName("img", )).click();
+        sectionCampains.findElement(By.className("link")).click();
         WebElement itemPage = browser.findElement(By.id("box-product"));
         //get info for element on itemPage
         String discountItemPrise = itemPage.findElement(By.className("regular-price")).getText();
+        String discountItemColor = itemPage.findElement(By.className("regular-price")).getCssValue("color");
         String regularItemPrise = itemPage.findElement(By.className("campaign-price")).getText();
+        String regularItemColor = itemPage.findElement(By.className("campaign-price")).getCssValue("color");
         String itemName = itemPage.findElement(By.tagName("h1")).getText();
 
         //verify names are correct on both pages
         assertEquals(itemName, itemNameMain);
         //verify discount prise  are correct on both pages
         assertEquals(discountItemPrise, discountMainPrise);
+        //color
+        assertEquals(discountMainColor, colorValue.get("greyOnMain"));
+        assertEquals(discountItemColor, colorValue.get("greyOnItem"));
         //verify regular prise are correct on both pages
         assertEquals(regularItemPrise, regularMainPrise);
+        //color
+        assertEquals(regularMainColor, colorValue.get("redOnMain"));
+        assertEquals(regularItemColor, colorValue.get("redOnItem"));
     }
 
-
-    @AfterClass
-    public static void stop(){
+    @After
+    public void stop(){
         browser.quit();
         browser = null;
     }
 
-    boolean areElementsPresent(By element){
-        return  browser.findElements(element).size() > 0;
-    }
-
-    void login_as(String user, String pass){
-        WebElement loginForm = browser.findElement(By.id("box-login"));
-        loginForm.findElement(By.name("username")).sendKeys(user);
-        loginForm.findElement(By.name("password")).sendKeys(pass);
-        loginForm.findElement(By.name("login")).click();
+    @Parameterized.Parameters
+    public static WebDriver[] driver(){
+        return new WebDriver[]{new ChromeDriver(), new FirefoxDriver(), new InternetExplorerDriver()};
+        //return new WebDriver[]{new ChromeDriver()};
     }
 }
